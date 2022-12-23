c066516ad98cb19380ed339daf06f513d92fc4be
morefigs <morefigs@gmail.com>
2022-12-23 11:16:25 +0100

    Made example run faster

--- examples/start_stop.py
+++ examples/start_stop.py
@@ -1,8 +1,8 @@
 """
-Count at half second time steps, delaying by 4 counts and stopping after 8 counts (including those skipped).
+Count at quarter second time steps, delaying by 4 counts and stopping after 8 counts (including those skipped).
 """
 from timed_count import timed_count
 
 
-for count in timed_count(0.5, start=4, stop=8):
+for count in timed_count(0.25, start=4, stop=8):
     print(count)
