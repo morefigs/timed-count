e075f78b1e90a6c6756855debc3af17666c52435
morefigs <morefigs@gmail.com>
2022-12-25 11:03:12 +0100

    Use absolute value of temporal resolution

--- timed_count/timed_count.py
+++ timed_count/timed_count.py
@@ -12,28 +12,25 @@ from timed_count.cls_timed_count import TimedCount
 def timed_count(period: float,
                 start: int = 0,
                 stop: Optional[int] = None,
-                temporal_resolution_exponent: int = -4) -> Iterator[TimedCount]:
+                temporal_resolution: float = 0.0001) -> Iterator[TimedCount]:
     """
     A generator function that returns an iterator that delays each iteration by the specified time period. It can be
     used to execute code at a precise frequency.
     :param period: The interval period, in seconds.
     :param start: The number of time counts to delay starting by.
     :param stop: The number of time counts to automatically stop after.
-    :param temporal_resolution_exponent: The temporal resolution exponent to use when blocking the next iteration. A
-    lower value will give higher resolution and precision but result in higher CPU usage. Use caution lowering below the
-    default value. This precision is for an individual iteration only, there is no cumulative time error over multiple
-    iterations.
+    :param temporal_resolution: The temporal resolution (or time error) to use when waiting for the next iteration, in
+    seconds. A smaller value will give higher resolution and precision but result in higher CPU usage. Use caution
+    lowering below the default value. This precision is for an individual iteration only, there is no cumulative time
+    error over multiple iterations.
     """
     index = start
 
-    # E.g. 10 ** -4 = 0.0001
-    cpu_sleep_s = 10 ** temporal_resolution_exponent
-
-    # count decimal places matches period decimal places
+    # `count` decimal places matches period decimal places
     count_dp = max(1, -Decimal(str(period)).as_tuple().exponent)
 
-    # time decimal places one less than temporal resolution
-    time_dp = max(count_dp, -temporal_resolution_exponent - 1)
+    # `time` decimal places one less than temporal resolution
+    time_dp = max(count_dp, -Decimal(str(temporal_resolution)).as_tuple().exponent - 1)
 
     with Stopwatch() as stopwatch:
         while True:
@@ -47,7 +44,7 @@ def timed_count(period: float,
             # Block the iteration until the next count time
             count = period * index
             while time < count:
-                sleep(cpu_sleep_s)
+                sleep(temporal_resolution)
                 time = stopwatch.time()
 
             timed_count_ = TimedCount(period, index, time, time_ready, count_dp, time_dp)
